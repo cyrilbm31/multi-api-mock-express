bf0e77aac3cb83a2830f20c3776a3feb3a38bb32
CyrilB <montes.cyril@gmail.com>
2020-05-04 14:58:29 +0200

    Ajouts de cross domain + api

--- mock-handler-ihm/main-server.js
+++ mock-handler-ihm/main-server.js
@@ -3,10 +3,19 @@ const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const port = 3000;
-const mainApi = "/mock-handler";
-
+const mainApi = "/mock-ihm-handler/";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use(function(req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin','*');
+         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+        res.setHeader('Access-Control-Expose-Headers','Content-Length,Content-Range');
+res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 
 //FIXME à faire une convention de nommage
 const fileServerPrefix = 'server-';
@@ -21,9 +30,16 @@ var tools = require('./utils/tools.js');
 tools.initLoad(localisationMockedApp, app);
 
 /**
+* Listes des apis
+*/
+const restartApi = 'restart';
+const login = 'login';
+
+
+/**
  * Permet de restart une api en lui passant son nom
  */
-app.get(mainApi, (req, res) => {
+app.get(mainApi + restartApi, (req, res) => {
     var serverFile = tools.findFile(req.query.servername);
     console.log('Handler mock for : ' + req.query.servername);
     if (serverFile) {
@@ -48,7 +64,14 @@ app.get(mainApi, (req, res) => {
     }
 });
 
+app.post(mainApi + login, (req, res) => {
+const dbConnect = require('./db/db-connection.json');
 
+
+    console.log('jai reach');
+// TODO FAIRE UNE FONCTION
+res.status(200).jsonp(dbConnect.connectionSuccess);
+             });
 
 // LAUNCH SERVER
 app.listen(port, () => {
